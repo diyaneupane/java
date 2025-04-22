8269dda72ddbc9dcbd8dcca19ea9ec08abc3f149
“diya” <diyaneupane439@gmail.com>
2025-04-22 09:06:39 +0545

    CHANGE

---
+++ task13.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class task13 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+           for(int i=1;i<=n;i++){
+                sum= sum+i;
+                               
+            }
+                System.out.println("the sbum is"+sum);
+                sc.close();
+   
+}
+}
